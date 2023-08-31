419ca1fb125987477ffcad1342e7052c191861f1
Gzuuu <jesue_gege@hotmail.com>
2023-08-31 17:29:29 -0300

    requisito 8 concluido

--- src/TrybeGames/Database/TrybeGamesDatabase.cs
+++ src/TrybeGames/Database/TrybeGamesDatabase.cs
@@ -61,8 +61,9 @@ public class TrybeGamesDatabase
     // 8. Crie a funcionalidade de buscar todos os diferentes Tipos de jogos dentre os jogos cadastrados
     public List<GameType> GetGameTypes()
     {
-        // Implementar
-        throw new NotImplementedException();
+        var filteredGamesTypes = from game in Games
+        select game.GameType;
+        return filteredGamesTypes.Distinct().ToList();
     }
 
     // 9. Crie a funcionalidade de buscar todos os estúdios de jogos junto dos seus jogos desenvolvidos com suas pessoas jogadoras
