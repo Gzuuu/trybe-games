1071aaf013480496ae2ab8eace260d17d3753b4a
Gzuuu <jesue_gege@hotmail.com>
2023-08-31 17:22:14 -0300

    requisito 7 concluido

--- src/TrybeGames/Database/TrybeGamesDatabase.cs
+++ src/TrybeGames/Database/TrybeGamesDatabase.cs
@@ -46,8 +46,16 @@ public class TrybeGamesDatabase
     // 7. Crie a funcionalidade de buscar todos os jogos junto do nome do estúdio desenvolvedor
     public List<GameWithStudio> GetGamesWithStudio()
     {
-        // Implementar
-        throw new NotImplementedException();                      
+        var filteredGames = from game in Games
+        from studio in GameStudios
+        where game.DeveloperStudio == studio.Id
+        select new GameWithStudio{
+            GameName = game.Name,
+            StudioName = studio.Name,
+            NumberOfPlayers = game.Players.Count,
+        };
+
+        return filteredGames.ToList();                    
     }
     
     // 8. Crie a funcionalidade de buscar todos os diferentes Tipos de jogos dentre os jogos cadastrados
